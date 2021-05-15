5a7a9fca325882cfdf90573573c87661c0e463fc
madruga665 <madruga665@hotmail.com>
2021-05-15 04:02:28 -0300

    Add style no componente MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import './styles/MovieCard.css'
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     const { title, storyline } = movie;
     return (
-      <div data-testid="movie-card">
+      <div className="movie-card" data-testid="movie-card">
         <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
         <p className="movie-card-storyline">{storyline}</p>
         <Link to={ `movies/${movie.id}` }>VER DETALHES</Link>
