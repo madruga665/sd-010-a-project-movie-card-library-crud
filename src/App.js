155b73b8994cb49477c8ee9b80721b7c7b41a5a7
madruga665 <madruga665@hotmail.com>
2021-05-15 04:08:11 -0300

    Add componente Header e Header.css

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Switch>
         <Route exact path="/" component={ MovieList } />
         <Route path="/movies/new" component={ NewMovie } />
