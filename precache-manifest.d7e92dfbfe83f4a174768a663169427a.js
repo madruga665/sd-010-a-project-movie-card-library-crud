44a505618143e1abd0789ee713e9f3a4640f44e8
Luciano Amâncio <madruga665@hotmail.com>
2021-10-04 04:52:52 -0300

    Updates

---
+++ precache-manifest.d7e92dfbfe83f4a174768a663169427a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe28b4543d628eb919ccca45827184e",
+    "url": "/trybe-projeto-movie-card-library-crud/index.html"
+  },
+  {
+    "revision": "ef145caaf68c3b3a096d",
+    "url": "/trybe-projeto-movie-card-library-crud/static/css/main.17b5920a.chunk.css"
+  },
+  {
+    "revision": "312199699346c87d8d94",
+    "url": "/trybe-projeto-movie-card-library-crud/static/js/2.24ab25d8.chunk.js"
+  },
+  {
+    "revision": "ef145caaf68c3b3a096d",
+    "url": "/trybe-projeto-movie-card-library-crud/static/js/main.8381f866.chunk.js"
+  },
+  {
+    "revision": "b575f4f131d1a05c9805",
+    "url": "/trybe-projeto-movie-card-library-crud/static/js/runtime-main.87c39923.js"
+  },
+  {
+    "revision": "e54442d92ad8ba256704c694616b5cee",
+    "url": "/trybe-projeto-movie-card-library-crud/static/media/bg.e54442d9.png"
+  },
+  {
+    "revision": "66463b37b9ed39da73c56f5d68b110a1",
+    "url": "/trybe-projeto-movie-card-library-crud/static/media/unicorn.66463b37.gif"
+  }
+]);
